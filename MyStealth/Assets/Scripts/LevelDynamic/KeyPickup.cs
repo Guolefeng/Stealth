7b04c41cf05d686ce43c9483bf3f903b463b3a53
郭乐峰 <18341894356@163.com>
2017-04-06 16:06:06 +0800

    钥匙卡设定

---
+++ MyStealth/Assets/Scripts/LevelDynamic/KeyPickup.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyPickup : MonoBehaviour {
+
+	public AudioClip keyGrab;                       // 播放拾取钥匙卡音效的音频剪辑
+
+
+	private GameObject player;                      // 引用玩家对象
+	private PlayerInventory playerInventory;        // 引用"PlayerInventory"脚本
+
+
+	void Awake ()
+	{
+		// 获取引用对象
+		player = GameObject.FindGameObjectWithTag(Tags.player);
+		playerInventory = player.GetComponent<PlayerInventory>();
+	}
+
+
+	void OnTriggerEnter (Collider other)
+	{
+		// 如果是玩家碰撞触发器
+		if(other.gameObject == player)
+		{
+			// 在钥匙卡位置播放拾取音效
+			AudioSource.PlayClipAtPoint(keyGrab, transform.position);
+
+			// 判定玩家已拿到钥匙卡
+			playerInventory.hasKey = true;
+
+			// 摧毁钥匙卡
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ MyStealth/Assets/Scripts/Player/PlayerInventory.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour {
+
+	public bool hasKey;         // 这个变量用于判断玩家是否拾取了钥匙卡
+}
